df4ffa3e5009a90bf7312990106b9635ba8814fb
Flor <fdutari2@gmail.com>
2019-02-13 14:05:44 +0100

    tenemos el screen splash hecho

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
 'use-strict'
+
+const main = () => {
+    
+    const buildDom = (html) => {
+        const main = document.querySelector('main');
+        main.innerHTML = html;
+        return main;
+    };
+
+    const buildSplashScreen = () => {
+        const splashScreen = buildDom(`
+        <section class="splash-screen">
+        <h1>Eternal Enemies</h1>
+        <button>Start</button>
+        </section>
+        `);
+
+    };
+
+    buildSplashScreen();
+
+
+
+};
+window.addEventListener('load', main);
